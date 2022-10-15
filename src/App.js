f0e90de7b31feba91ae8a47fdd0bc1a3235988a6
afollack <andreafollack@gmail.com>
2022-10-15 00:42:45 -0700

    added footer data

--- src/App.js
+++ src/App.js
@@ -8,6 +8,30 @@ function App() {
       <div className="container-fluid">
         <WeatherSearch />
       </div>
+      <div className="footer">
+        <small>
+          WEATHER APP{" "}
+          <span role="img" aria-label="two hearts emoji">
+            💕
+          </span>{" "}
+          Coded by Andrea Follack
+        </small>
+        <br />
+        <a
+          href="https://github.com/afollack/weather-app-js-shecodes"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="bi bi-github"></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/andreafollack/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="bi bi-linkedin"></i>
+        </a>
+      </div>
     </div>
   );
 }
