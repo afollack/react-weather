59ae84a98ecc0ab54ac32a14ffb8eb43d007dedb
afollack <andreafollack@gmail.com>
2022-10-15 21:34:07 -0700

    Moved footer into it's own component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Weather from "./Weather";
+import Footer from "./Footer";
 
 import "./App.css";
 
@@ -8,22 +9,7 @@ function App() {
       <div className="container-fluid">
         <Weather />
       </div>
-      <div className="footer pt-2 d-flex justify-content-end">
-        <a
-          href="https://github.com/afollack/weather-app-js-shecodes"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <i className="bi bi-github ps-1"></i>
-        </a>
-        <a
-          href="https://www.linkedin.com/in/andreafollack/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <i className="bi bi-linkedin p-1"></i>
-        </a>
-      </div>
+      <Footer />            
     </div>
   );
 }
